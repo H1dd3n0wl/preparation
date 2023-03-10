e92270bc1339b655e1aa150418fab7c551cc9e74
Andrey Zhukov <sviter1232@gmail.com>
2023-03-10 23:34:01 +0300

    First task solution

---
+++ romanToInt/Solution.h
@@ -0,0 +1,16 @@
+//
+// Created by andrey on 10.3.23.
+//
+
+#ifndef LEETCODE_SOLUTION_H
+#define LEETCODE_SOLUTION_H
+
+#include <iostream>
+
+class Solution {
+public:
+    int romanToInt(const std::string& s);
+};
+
+
+#endif //LEETCODE_SOLUTION_H
