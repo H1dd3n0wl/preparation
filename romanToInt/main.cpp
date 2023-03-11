21662c2d52f7f2786d5d058c06eee8b592039bbd
Andrey Zhukov <sviter1232@gmail.com>
2023-03-11 18:30:12 +0300

    changed tests in main.cpp

--- romanToInt/main.cpp
+++ romanToInt/main.cpp
@@ -1,8 +1,11 @@
-#include <iostream>
-
+#include <cassert>
+#include <map>
 #include "Solution.h"
 
 int main() {
     Solution sol;
-    sol.romanToInt("INI");
+    std::map<std::string, int> tests = {{"III", 3}, {"LVIII", 58}, {"MCMXCIV", 1994}};
+    for (auto& [test, ans] : tests) {
+        assert(sol.romanToInt(test) == ans);
+    }
 }
